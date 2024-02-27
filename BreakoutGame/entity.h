6de79bdb5756fc18b9fede47c9dc380839ae0e53
dianaiaurum <iaurumdianaotilia@gmail.com>
2024-02-27 15:33:58 +0200

    created entity class
    entity class is the base class for the elements of the game

--- BreakoutGame/entity.h
+++ BreakoutGame/entity.h
@@ -1,1 +1,22 @@
 #pragma once
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <SFML/Graphics.hpp>
+
+//Abstract base class to represent any graphical entity on the screen
+class entity {
+protected:
+	sf::Sprite sprite;
+
+public:
+	//Pure member functions -> concrete graphical entities must implement these functions
+	//The update member function will compute the new possition, appearance, etc of the object
+	//The display member function will display the object in the game window
+	virtual void update() = 0;
+	virtual void draw(sf::RenderWindow& window) = 0;
+	//Virtual destructor
+	virtual ~entity() {}
+};
+
+#endif  // ! ENTITY_H
