3b7bc5b88e5494358ee76c20a9fa5f190e19bbd2
dianaiaurum <iaurumdianaotilia@gmail.com>
2024-02-28 16:15:56 +0200

    bricks

--- BreakoutGame/collisions.cpp
+++ BreakoutGame/collisions.cpp
@@ -1,12 +1,21 @@
 #include "collisions.h"
 
+<<<<<<< Updated upstream
 bool is_interacting(const entity& e1, const entity& e2) {
     auto box1 = e1.get_bounding_box();
+=======
+bool is_interacting(const entity& e1, const entity& e2){
+    auto box1 = e1.get_bounding_box(); 
+>>>>>>> Stashed changes
     auto box2 = e2.get_bounding_box();
     return box1.intersects(box2);
 }
 
+<<<<<<< Updated upstream
 void handle_collision(ball& b, const paddle& p) {
+=======
+void handle_collision(ball& b, const paddle& p){
+>>>>>>> Stashed changes
     if (is_interacting(b, p)) {
         //make the ball bounce upwards
         b.move_up();
